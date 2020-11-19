846c6f93a633f7882b0feebaf9f5cfaf06578787
ccx_huang <1311607595@qq.com>
2020-11-19 15:21:29 +0800

    20201119 hello world

---
+++ spring-cloud-client/cloud-demo-hello/src/main/java/spring/demo/hello/Application.java
@@ -0,0 +1,26 @@
+package spring.demo.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author huazai
+ * @date 2020/11/19 14:51
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+}
--- spring-cloud-client/cloud-service-payment/src/main/java/spring/test/payment/test.java
+++
@@ -1,8 +0,0 @@
-package spring.test.payment;
-
-/**
- * @author huazai
- * @date 2020/10/22 15:23
- */
-public class test {
-}
